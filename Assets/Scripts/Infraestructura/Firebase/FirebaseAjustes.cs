3322832f5dd5216c7faf0b58765be498a97cfca7
Adri <adrianrodry55@gmail.com>
2025-10-28 12:15:59 +0100

    Entrega_1.6

---
+++ Assets/Scripts/Infraestructura/Firebase/FirebaseAjustes.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+// Guarda apiKey y projectId de tu proyecto Firebase.
+// Así no las quemo en código y las puedo cambíar fácil en el inspector.
+[CreateAssetMenu(fileName = "FirebaseAjustes", menuName = "Config/FirebaseAjustes")]
+public class FirebaseAjustes : ScriptableObject
+{
+    [Tooltip("apiKey de Firebase")]
+    public string apiKey;
+
+    [Tooltip("ID proyecto de Firebase")]
+    public string projectId;
+}
